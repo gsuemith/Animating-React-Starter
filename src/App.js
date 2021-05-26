afcb7c4c3fca6a70fc5f2693c09fbd8d45bcc450
Garrick Suemith <garrick.suemith@gmail.com>
2021-05-26 09:07:13 -0400

    transitions

--- src/App.js
+++ src/App.js
@@ -1,19 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSpring, animated } from 'react-spring'
 import logo from './logo.svg';
 import './App.css';
 import Toggle from './Toggle';
+import Nav from './Nav';
+import Checkout from './Checkout';
 
 const App = () => {
+  const [isNavOpen, setNavOpen] = useState(false);
+  const navAnimation = useSpring({ 
+    transform: isNavOpen ? 
+    `translate3d(0,0,0) scale(1)`
+      :
+    `translate3d(100%,0,0) scale(0.3)`
+  })
   const fade = useSpring({from: {opacity: 0},opacity: 1 })
  
   return (
     <animated.div className="App" style={fade}>
       <header className="App-header">
         <img src={logo} className="logo" alt="logo"/>
-        <button className="menu-button">Menu</button>
+        <button 
+          onClick={() => setNavOpen(!isNavOpen)}
+          className="menu-button"
+        >Menu</button>
+        {/* <Nav style={navAnimation}/> */}
       </header>
-      <Toggle />
+      <main>
+        <Toggle />
+        <Checkout isOpen={isNavOpen}/>
+      </main>
     </animated.div>
   );
   
--- src/Toggle.js
+++ src/Toggle.js
@@ -1,26 +1,66 @@
 import React, { useState } from 'react'
-import { useSpring, animated } from 'react-spring'
+import { useTransition, animated } from 'react-spring'
 
 const Toggle = () => {
-  const [isToggled, setToggle] = useState(false);
-  const fade = useSpring({
-    // opacity: isToggled ? 1 : 0,
-    color: isToggled ? 'tomato' : 'green',
-    // fontSize: isToggled ? '2rem': '10em',
-    transform: isToggled ? 'translate3d(0,0,0)' : 'translate3d(-50px, -50px, 0)'
+  const [items, setItems] = useState([
+    {letter: "S", key: 1},  
+    {letter: "c", key: 2},  
+    {letter: "o", key: 3},  
+    {letter: "t", key: 4},  
+    {letter: "t", key: 5},  
+  ]);
+  const transition = useTransition(items, item => item.key, {
+    from: { opacity: 0},
+    enter: { opacity: 1},
+    leave: { opacity: 0},
   })
 
   return (
     <div>
-      <animated.h1 style={fade}>
-        {/* {isToggled ? "Hello Animated World!" : "Good Bye!"} */}
-        Hello
-      </animated.h1>
-      <button onClick={() => setToggle(!isToggled)}>
+      {transition.map(({ item, key, props }) => (
+        
+        <animated.h1 key={key} style={props}>
+          {item.letter}
+        </animated.h1>
+        
+      ))}
+      <button onClick={() => setItems([{
+        letter: "S", key: 1
+      }])}>
         Toggle
       </button>
     </div>
   )
 }
 
+// const AnimatedTitle = animated.h1
+
 export default Toggle
+
+// const Toggle = () => {
+//   const [isToggled, setToggle] = useState(false);
+//   const transition = useTransition(isToggled, null, {
+//     from: { opacity: 0, position: 'absolute'},
+//     enter: { opacity: 1},
+//     leave: { opacity: 0},
+//   })
+
+//   return (
+//     <div style={{position: 'relative'}}>
+//       {transition.map(({ item, key, props }) => (
+//         item ? (
+//         <animated.h1 key={key} style={props}>
+//           Hello
+//         </animated.h1>
+//         ) : (
+//         <animated.h1 key={key} style={props}>
+//           World
+//         </animated.h1>
+//         )
+//       ))}
+//       <button onClick={() => setToggle(!isToggled)}>
+//         Toggle
+//       </button>
+//     </div>
+//   )
+// }
